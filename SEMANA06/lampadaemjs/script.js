ca79160f7682589d5cd27fb3e4da38081792da7d
ViniciusMoresqui <viniciusgiratomoresqui@gmail.com>
2025-03-27 22:10:52 -0300

    Aula Calculadora basíca com js
    Hoje foi feito em aula, uma calculadora com front em HTML e foi feito as parte das calculo com js.

---
+++ SEMANA06/lampadaemjs/script.js
@@ -0,0 +1,9 @@
+function ligar(){
+    let lampada = document.getElementById('lampada'); // a tag id=lampada
+    lampada.src = "imagens/lampada-acesa.jpg";
+    document.get
+}
+
+function desligar(){
+    document.getElementById('lampada').src = "imagens/lampada-apagada.png";
+}
