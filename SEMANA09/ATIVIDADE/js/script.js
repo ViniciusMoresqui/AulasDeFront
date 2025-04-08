5355994933f13befe9fc2859a09dda012c83cacc
ViniciusMoresqui <viniciusgiratomoresqui@gmail.com>
2025-04-08 20:39:38 -0300

    usando vetor com lista

---
+++ SEMANA09/ATIVIDADE/js/script.js
@@ -0,0 +1,31 @@
+// addEvemtListener()
+
+const entrada = document.getElementById('entrada');
+const botao = document.getElementById('add');
+const lista = document.getElementById('lista');
+const remover = document.getElementById('remover');
+
+const vetor = [];
+
+botao.addEventListener('click', function(){
+    let item = entrada.value;
+    vetor.push(item);
+    entrada.value = "";
+    console.log(vetor);
+    saida();
+});
+
+function saida(){
+    lista.innerHTML = "";
+
+    for(let i = 0; i < vetor.length; i++){
+        let li = document.createElement("li");
+        li.textContent = vetor[i];
+        lista.appendChild(li); 
+    } 
+}
+
+botao.addEventListener('click', function(){
+    vetor.pop();
+    saida();
+});
